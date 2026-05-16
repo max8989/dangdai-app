972153a893ccb0a1edb2a0bc64358f12616454d7
Maxime Gagne <max.8989@hotmail.com>
2026-05-16 00:15:34 -0400

    fix safearea

--- dangdai-mobile/components/Provider.tsx
+++ dangdai-mobile/components/Provider.tsx
@@ -3,6 +3,7 @@ import { TamaguiProvider, Theme, type TamaguiProviderProps } from 'tamagui'
 import { ToastProvider, ToastViewport } from '@tamagui/toast'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { SafeAreaProvider, initialWindowMetrics } from 'react-native-safe-area-context'
 import { CurrentToast } from './CurrentToast'
 import { config } from '../tamagui.config'
 import { queryClient } from '../lib/queryClient'
@@ -16,29 +17,31 @@ export function Provider({
 
   return (
     <GestureHandlerRootView style={styles.root}>
-      <TamaguiProvider
-        config={config}
-        defaultTheme={colorScheme}
-        {...rest}
-      >
-        {/* Theme wrapper enables reactive switching -- defaultTheme alone only sets initial value */}
-        <Theme name={colorScheme}>
-          <QueryClientProvider client={queryClient}>
-            <ToastProvider
-              swipeDirection="horizontal"
-              duration={12000}
-              native={[
-                // uncomment the next line to do native toasts on mobile. NOTE: it'll require you making a dev build and won't work with Expo Go
-                // 'mobile'
-              ]}
-            >
-              {children}
-              <CurrentToast />
-              <ToastViewport top="$8" left={0} right={0} />
-            </ToastProvider>
-          </QueryClientProvider>
-        </Theme>
-      </TamaguiProvider>
+      <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+        <TamaguiProvider
+          config={config}
+          defaultTheme={colorScheme}
+          {...rest}
+        >
+          {/* Theme wrapper enables reactive switching -- defaultTheme alone only sets initial value */}
+          <Theme name={colorScheme}>
+            <QueryClientProvider client={queryClient}>
+              <ToastProvider
+                swipeDirection="horizontal"
+                duration={12000}
+                native={[
+                  // uncomment the next line to do native toasts on mobile. NOTE: it'll require you making a dev build and won't work with Expo Go
+                  // 'mobile'
+                ]}
+              >
+                {children}
+                <CurrentToast />
+                <ToastViewport top="$8" left={0} right={0} />
+              </ToastProvider>
+            </QueryClientProvider>
+          </Theme>
+        </TamaguiProvider>
+      </SafeAreaProvider>
     </GestureHandlerRootView>
   )
 }
